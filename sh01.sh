1a09e2f95bd277bcfcdaea48ec1815c37a49af6c
wenyuanhust <wenyuanhust@qq.com>
2016-08-20 18:02:11 +0800

    bird 13 chapter sh01

---
+++ sh01.sh
@@ -0,0 +1,8 @@
+#Program:
+#	This program shows "Hello World!" in your screen.
+#History
+#2016/08/20 WenYuan First release
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+echo -e "Hello World!\a \n"
+exit 0
